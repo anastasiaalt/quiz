70701fdbcabba13542ca924e8ee9f1919a39375b
Anastasia Alt <anastasia.alt@gmail.com>
2016-02-09 14:28:31 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,38 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Answer.destroy_all
+Submission.destroy_all
 Option.destroy_all
 Student.destroy_all
 Instructor.destroy_all
 Question.destroy_all
 Quiz.destroy_all
 
-a1 = Answer.create({name: "Washington, D.C", explanation: "Washington, D.C. became the capital in 1790"})
-a2 = Answer.create({name: "Philadelphia, PA", explanation: "The Constitution was written during the Philadelphia Convention—now known as the Constitutional Convention—which convened from May 25 to September 17, 1787. It was signed on September 17, 1787"})
-a3 = Answer.create({name: "Provincetown, MA", explanation: "The Pilgrims first landed, however, near the site of modern Provincetown on the tip of Cape Cod in November 1620 before moving to Plymouth"})
+inst1 = Instructor.create({first_name: "Matt", last_name: "Short", username: "mattshort", password: "1234"})
+stud1 = Student.create({first_name: "Anastasia", last_name: "Alt", username: "anastasiaalt", password: "1234"})
 
-o1 = Option.create({name: "New York, NY"})
-o2 = Option.create({name: "Miami, FL"})
-o3 = Option.create({name: "Boston, MA"})
-o4 = Option.create({name: "Hartford, CT"})
-o5 = Option.create({name: "New Haven, CT"})
-o6 = Option.create({name: "Pittsburg, PA"})
-o7 = Option.create({name: "Plymouth, MA"})
-o8 = Option.create({name: "Cambridge, MA"})
-o9 = Option.create({name: "Portland, ME"})
+q1 = Question.create({ask: "What is the current capital of the United States?", explanation: "Washington, D.C. became the capital in 1790"})
+q2 = Question.create({ask: "Where was the constitution signed?", explanation: "The Constitution was written during the Philadelphia Convention—now known as the Constitutional Convention—which convened from May 25 to September 17, 1787. It was signed on September 17, 1787"})
+q3 = Question.create({ask: "Where did the Pilgrims land?", explanation: "The Pilgrims first landed, however, near the site of modern Provincetown on the tip of Cape Cod in November 1620 before moving to Plymouth"})
 
-s1 = Student.create({first_name: "Anastasia", last_name: "Alt", username: "anastasiaalt", password: "1234"})
+o1 = Option.create({name: "New York, NY", correct: false, question: q1})
+o2 = Option.create({name: "Miami, FL", correct: false, question: q2})
+o3 = Option.create({name: "Boston, MA", correct: false, question: q3})
+o4 = Option.create({name: "Hartford, CT", correct: false, question: q1})
+o5 = Option.create({name: "New Haven, CT", correct: false, question: q2})
+o6 = Option.create({name: "Pittsburg, PA", correct: false, question: q3})
+o7 = Option.create({name: "Plymouth, MA", correct: false, question: q1})
+o8 = Option.create({name: "Cambridge, MA", correct: false, question: q2})
+o9 = Option.create({name: "Portland, ME", correct: false, question: q3})
+o10 = Option.create({name: "Washington, D.C", correct: false, question: q1})
+o11 = Option.create({name: "Philadelphia, PA", correct: false, question: q2})
+o12 = Option.create({name: "Provincetown, MA", correct: false, question: q3})
 
-i1 = Instructor.create({first_name: "Matt", last_name: "Short", username: "mattshort", password: "1234"})
+s1 = Submission.create({student: stud1, option: o10})
+s2 = Submission.create({student: stud1, option: o11})
+s3 = Submission.create({student: stud1, option: o12})
 
-quest1 = Question.create({ask: "What is the current capital of the United States?", choice_a: o1, choice_b: o2, choice_c: o3, choice_d: a1})
-quest2 = Question.create({ask: "Where was the constitution signed?", choice_a: o4, choice_b: o5, choice_c: o6, choice_d: a2})
-quest3 = Question.create({ask: "Where did the Pilgrims land?", choice_a: o7, choice_b: o8, choice_c: o9, choice_d: a3})
+quiz1 = Quiz.create({assigned: Date.parse("Feb 9, 2016"), instructor: inst1, question: q1, })
 
-quiz1 = Quiz.create({assigned: Date.parse("Feb 9, 2016"), instructor: i1, question1: quest1, question2: quest2, question3: quest3})
 
 
